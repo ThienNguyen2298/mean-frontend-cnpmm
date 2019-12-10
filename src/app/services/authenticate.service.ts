61be30aef540d4b66ac3fad1b5c6257d5fcb8e4d
Quang Nam <quangnamute.98@gmail.com>
2019-12-10 15:08:00 +0700

    zc

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { JwtHelperService } from '@auth0/angular-jwt';
+// import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,7 +8,7 @@ export class AuthenticateService {
   url = 'http://localhost:8001';
   constructor(
     private http: HttpClient,
-    public jwtHelper: JwtHelperService
+    // public jwtHelper: JwtHelperService
   ) { }
 
   loginFacebook() {
@@ -26,13 +26,4 @@ export class AuthenticateService {
   logout() {
     window.location.href = this.url + '/auth/logout';
   }
-
-
-  isAuthenticated(): boolean {
-    const token = localStorage.getItem('token');
-    // Check whether the token is expired and return
-    // true or false
-    return !this.jwtHelper.isTokenExpired(token);
-  }
-
 }
